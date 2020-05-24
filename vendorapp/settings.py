ee1ccd5ce3f2097306e687c8e4ed8832d0960c6c
bruce619 <chimuanyaibecheozor@gmail.com>
2020-05-24 12:31:53 +0100

    Initial Commit

--- vendorapp/settings.py
+++ vendorapp/settings.py
@@ -164,20 +164,21 @@ SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", '')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', '')
-S3_USE_SIGV4 = os.environ.get('S3_USE_SIGV4')
-AWS_LOCATION = 'static'
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", '')
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", '')
+# AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", '')
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', '')
+# S3_USE_SIGV4 = os.environ.get('S3_USE_SIGV4')
+# AWS_LOCATION = 'static'
 # STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#
 DEFAULT_FILE_STORAGE = 'vendorapp.storage_backends.MediaStorage'
+#
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
