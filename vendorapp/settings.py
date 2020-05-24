3feb41f5c90a9f71135d5e6191b2991c9fdc4a8e
bruce619 <chimuanyaibecheozor@gmail.com>
2020-05-24 05:33:30 +0100

    Initial Commit

--- vendorapp/settings.py
+++ vendorapp/settings.py
@@ -179,8 +179,8 @@ DEFAULT_FILE_STORAGE = 'vendorapp.storage_backends.MediaStorage'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 django_heroku.settings(locals())
