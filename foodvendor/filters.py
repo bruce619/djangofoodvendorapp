07d18ed1aeec18742bcc61c56e831744efd8b444
bruce619 <chimuanyaibecheozor@gmail.com>
2020-05-26 10:41:39 +0100

    Initial Commit

--- foodvendor/filters.py
+++ foodvendor/filters.py
@@ -3,6 +3,7 @@ from .models import Menu, MenuItem, Order
 
 
 class MenuFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(lookup_expr='icontains')
 
     class Meta:
         model = Menu
