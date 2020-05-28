5b968ff015cb3a8bf762b9fb774f7122b1a12b12
bruce619 <chimuanyaibecheozor@gmail.com>
2020-05-28 21:31:17 +0100

    Initial Commit

--- foodvendor/update_task.py
+++ foodvendor/update_task.py
@@ -1,4 +1,18 @@
 from background_task import background
+from django.forms import model_to_dict
+from django.core.serializers.json import DjangoJSONEncoder
+from django.db.models import Model
+import json
+
+
+class ExtendedEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+
+        if isinstance(o, Model):
+            return model_to_dict(o)
+
+        return super().default(o)
 
 
 @background(schedule=60)
@@ -6,3 +20,4 @@ def update_date(obj, datetimecreated, new_date):
     obj.objects.get(datetimecreated=datetimecreated)
     obj.objects.update(datetimecreated=new_date)
     obj.save()
+    json.dumps(obj, cls=ExtendedEncoder)
