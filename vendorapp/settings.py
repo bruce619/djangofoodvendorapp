24a263035630df7a7f39eaeeeefb93bb86ab090f
bruce619 <chimuanyaibecheozor@gmail.com>
2020-05-29 22:44:16 +0100

    Initial Commit

--- vendorapp/settings.py
+++ vendorapp/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'background_task',
     'storages',
     'django_filters',
     'bootstrapform',
